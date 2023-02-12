3fff838bc04f1e0296ba889f80d6cc9aa2636216
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-02-12 11:45:35 +0300

    updating faq

--- medtracker/src/pages/Faq.jsx
+++ medtracker/src/pages/Faq.jsx
@@ -8,7 +8,7 @@ export default function Faq() {
        
     })
   return (
-    <div className='w-[100%] flex flex-col gap-10 mb-[20%]'>
+    <div className='w-[100%] flex flex-col gap-10'>
         <div className="flex flex-col gap-10">
             <div className="flex justify-center items-center">
                 <h1 className="text-5xl font-bold my-[5%]">
@@ -24,6 +24,19 @@ export default function Faq() {
                 </div>
             </div>
         </div>
+        <div className="flex mt-[20%] bg-[#FFF7F1]">
+                <div className="flex-1 my-auto text-xl font-[400]">
+                    Learn More About The World Medical Card® And How To Use It
+                </div>
+                <div className="flex-1 m-auto ">
+                    <button className="bg-[#a81515]  text-white p-3 item-center rounded-md">
+                    Watch Video
+                    </button>
+                </div>
+                <div className="flex-1">
+                    <img className="mt-[-100px]" src={img7} alt="" />
+                </div>
+            </div>
 
     </div>
   )
