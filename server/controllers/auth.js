9ab53a0f16e6b8c7faa783953526623f79bf911d
Segnicho <segnidesta60@gmail.com>
2023-02-15 09:08:10 +0300

    changed auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,56 +1,37 @@
 import User from "../models/User.js";
 
-import cryptojs from "crypto-js";
-
-import Jwt from "jsonwebtoken";
-export const registerUser = async (req, res) => {
-  const hashPassword = cryptojs.AES.encrypt(
-    req.body.password,
-    process.env.PASS
-  ).toString();
-
-  const newUser = User({
-    firstname: req.body.firstname,
-    email: req.body.email,
-    password: hashPassword,
-  });
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+export const registerUser = async (req, res, next) => {
   try {
-    const user = await newUser.save();
-    res.status(201).json(user);
+    const salt = bcrypt.genSaltSync(11);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+    await newUser.save();
+    res.status(200).send("user has been created");
   } catch (err) {
-    res.status(500).json(err);
+    next(err);
   }
 };
-
-export const loginUser = async (req, res) => {
+export const loginUser = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(500).json("Invalid credentials");
-    const hashedPassword = cryptojs.AES.decrypt(
-      user.password,
-      process.env.PASS
-    );
-
-    const userPassword = hashedPassword.toString(cryptojs.enc.Utf8);
-
-    if (userPassword !== req.body.password) {
-      return res.status(401).json("Wrong credentials");
-    }
-
-    const accessToken = Jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-
-      process.env.JWT,
-
-      { expiresIn: "2d" }
-    );
-    const { password, ...otherInfo } = user._doc;
-
-    res.status(200).json({ ...otherInfo, accessToken });
+    if (!user)
+      return next(handleError({ status: 404, message: "user not found" }));
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isCorrect)
+      return next(
+        handleError({ status: 400, message: "Incorrect email or password!" })
+      );
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password, ...noPassword } = user._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(noPassword);
   } catch (err) {
-    res.status(500).json(err);
+    next(err);
   }
 };
